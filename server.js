bf9d1802dd17159b613cbecc5138af9f5ca1ce9c
Nicolas Cazco <ncazco@est.ups.edu.ec>
2021-01-25 12:04:12 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,31 @@
+require("./server/config/config");
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Configuracion global de rutas
+app.use(require('./server/routes/index'));
+
+mongoose.connect(
+  process.env.URLDB,
+  { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true },
+
+  (err, res) => {
+    if (err) throw err;
+
+    console.log("Base de datos ONLINE!");
+  }
+);
+
+app.listen(process.env.PORT, () => {
+  console.log("Escuchando en el puerto: ", process.env.PORT);
+});
---
+++ server/config/config.js
@@ -0,0 +1,37 @@
+// ===========================================
+//  Puerto
+// ===========================================
+process.env.PORT = process.env.PORT || 3000;
+
+// ===========================================
+//  Entorno
+// ===========================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; 
+
+// ===========================================
+//  Base de Datos
+// ===========================================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev'){
+    urlDB = "mongodb://localhost:27017/cocoa";
+} else {
+    urlDB = process.env.MONGO_URI;
+}00
+
+process.env.URLDB = urlDB;
+
+// ===========================================
+//  Vencimiento del Token
+// ===========================================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// ===========================================
+//  SEED de autenticación
+// ===========================================
+process.env.SEED = process.env.SEED || 'este-es-la-clave-en-dev';
